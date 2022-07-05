9b2cf042eb7c7f1937c634f9facb43f1292d2212
Pranav Prashar <prasherlucky@gmail.com>
2022-07-05 18:03:29 -0400

    Added Upload Component

---
+++ src/components/Upload/Upload.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Upload.scss";
+import NavBar from "../Navbar/Navbar";
+
+function Upload() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default Upload;
