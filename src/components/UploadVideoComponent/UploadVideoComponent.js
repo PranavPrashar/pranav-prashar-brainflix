f77f9b809cfba3d9b492028138a8c24c7657fe7a
Pranav Prashar <prasherlucky@gmail.com>
2022-07-09 17:10:26 -0400

    Updates to Upload page for tablet and desktop views

--- src/components/UploadVideoComponent/UploadVideoComponent.js
+++ src/components/UploadVideoComponent/UploadVideoComponent.js
@@ -10,7 +10,56 @@ function UploadVideoComponent() {
         <h2 className="uploadvideocomponent__header--large">Upload Video</h2>
       </div>
 
-      <div>
+      <div className="uploadvideocomponent__wrapper">
+        <div>
+          <p className="uploadvideocomponent__header--little">
+            video thumbnail
+          </p>
+          <div className="uploadvideocomponent__container">
+            <img
+              src={uploadImage}
+              alt="Video"
+              className="uploadvideocomponent__container--image"
+            />
+          </div>
+        </div>
+
+        <div className="uploadvideocomponent__form">
+          <form>
+            <label
+              className="uploadvideocomponent__form--label"
+              htmlFor="videotitle"
+            >
+              title your video
+            </label>
+            <textarea
+              id="videotitle"
+              className="uploadvideocomponent__form--textarea"
+              name="videotitle"
+              placeholder="Add a title to your video"
+              rows={2}
+            ></textarea>
+
+            <label
+              className="uploadvideocomponent__form--label"
+              htmlFor="videodescription"
+            >
+              Add a video description
+            </label>
+            {/* <form> */}
+            <textarea
+              id="videodescription"
+              className="uploadvideocomponent__form--textarea"
+              name="videodescription"
+              placeholder="Add a description to your video"
+              rows={6}
+              // cols={4}
+            ></textarea>
+          </form>
+        </div>
+      </div>
+
+      {/* <div>
         <p className="uploadvideocomponent__header--little">video thumbnail</p>
         <div className="uploadvideocomponent__container">
           <img
@@ -43,7 +92,7 @@ function UploadVideoComponent() {
           >
             Add a video description
           </label>
-          {/* <form> */}
+
           <textarea
             id="videodescription"
             className="uploadvideocomponent__form--textarea"
@@ -53,7 +102,7 @@ function UploadVideoComponent() {
             // cols={4}
           ></textarea>
         </form>
-      </div>
+      </div> */}
 
       <div className="uploadvideocomponent__buttons">
         <span className="uploadvideocomponent__buttons--publish">
