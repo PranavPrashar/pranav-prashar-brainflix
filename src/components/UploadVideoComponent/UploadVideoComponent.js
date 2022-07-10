8b35fff91c6fe1c324b9fb1925b9456c87d92ae8
Pranav Prashar <prasherlucky@gmail.com>
2022-07-09 22:20:44 -0400

    Handle submit for the upload button, Trying to figure out how to delay the redirect

--- src/components/UploadVideoComponent/UploadVideoComponent.js
+++ src/components/UploadVideoComponent/UploadVideoComponent.js
@@ -1,65 +1,86 @@
-import React from "react";
+import React, { Component } from "react";
 import "./UploadVideoComponent.scss";
 import publishIcon from "../../assets/icons/publish.svg";
 import uploadImage from "../../assets/images/Upload-video-preview.jpg";
+import { NavLink } from "react-router-dom";
 
-function UploadVideoComponent() {
-  return (
-    <div className="uploadvideocomponent">
-      <div className="uploadvideocomponent__header">
-        <h2 className="uploadvideocomponent__header--large">Upload Video</h2>
-      </div>
+function confirmPublish() {
+  alert("Video Published");
+}
 
-      <div className="uploadvideocomponent__wrapper">
-        <div>
-          <p className="uploadvideocomponent__header--little">
-            video thumbnail
-          </p>
-          <div className="uploadvideocomponent__container">
-            <img
-              src={uploadImage}
-              alt="Video"
-              className="uploadvideocomponent__container--image"
-            />
+class UploadVideoComponent extends Component {
+  state = {
+    // publishButtonState: false,
+    videoStatus: "",
+  };
+
+  handlePublishClick = (event) => {
+    window.confirm("Video Has been published");
+    // setTimeout(() => {
+    //   // event.preventDefault();
+    //   this.setState({ videoStatus: "Video Has been updated" });
+    // }, 2000);
+
+    // this.setState({videoStatus:"Video Has been updated"})
+  };
+  render() {
+    return (
+      <div className="uploadvideocomponent">
+        <div className="uploadvideocomponent__header">
+          <h2 className="uploadvideocomponent__header--large">Upload Video</h2>
+        </div>
+
+        <div className="uploadvideocomponent__wrapper">
+          <div>
+            <p className="uploadvideocomponent__header--little">
+              video thumbnail
+            </p>
+            <div className="uploadvideocomponent__container">
+              <img
+                src={uploadImage}
+                alt="Video"
+                className="uploadvideocomponent__container--image"
+              />
+            </div>
+          </div>
+
+          <div className="uploadvideocomponent__form">
+            <form>
+              <label
+                className="uploadvideocomponent__form--label"
+                htmlFor="videotitle"
+              >
+                title your video
+              </label>
+              <textarea
+                id="videotitle"
+                className="uploadvideocomponent__form--textarea"
+                name="videotitle"
+                placeholder="Add a title to your video"
+                rows={2}
+              ></textarea>
+
+              <label
+                className="uploadvideocomponent__form--label"
+                htmlFor="videodescription"
+              >
+                Add a video description
+              </label>
+              {/* <form> */}
+              <textarea
+                id="videodescription"
+                className="uploadvideocomponent__form--textarea"
+                name="videodescription"
+                placeholder="Add a description to your video"
+                rows={6}
+                // cols={4}
+              ></textarea>
+            </form>
+            <p className="confirmPublish">{this.state.videoStatus}</p>
           </div>
         </div>
 
-        <div className="uploadvideocomponent__form">
-          <form>
-            <label
-              className="uploadvideocomponent__form--label"
-              htmlFor="videotitle"
-            >
-              title your video
-            </label>
-            <textarea
-              id="videotitle"
-              className="uploadvideocomponent__form--textarea"
-              name="videotitle"
-              placeholder="Add a title to your video"
-              rows={2}
-            ></textarea>
-
-            <label
-              className="uploadvideocomponent__form--label"
-              htmlFor="videodescription"
-            >
-              Add a video description
-            </label>
-            {/* <form> */}
-            <textarea
-              id="videodescription"
-              className="uploadvideocomponent__form--textarea"
-              name="videodescription"
-              placeholder="Add a description to your video"
-              rows={6}
-              // cols={4}
-            ></textarea>
-          </form>
-        </div>
-      </div>
-
-      {/* <div>
+        {/* <div>
         <p className="uploadvideocomponent__header--little">video thumbnail</p>
         <div className="uploadvideocomponent__container">
           <img
@@ -104,19 +125,28 @@ function UploadVideoComponent() {
         </form>
       </div> */}
 
-      <div className="uploadvideocomponent__buttons">
-        <span className="uploadvideocomponent__buttons--publish">
-          <img
-            src={publishIcon}
-            alt="comment icon"
-            className="conversation__right--commentbutton--image"
-          ></img>
-          <p className="conversation__right--commentbutton--text">Publish</p>
-        </span>
-        <span className="uploadvideocomponent__buttons--cancel">Cancel</span>
+        <div className="uploadvideocomponent__buttons">
+          <span className="uploadvideocomponent__buttons--publish">
+            <img
+              src={publishIcon}
+              alt="comment icon"
+              className="conversation__right--commentbutton--image"
+            ></img>
+            <NavLink
+              to=""
+              className="conversation__right--commentbutton--text"
+              onClick={this.handlePublishClick}
+            >
+              <p className="conversation__right--commentbutton--text">
+                Publish
+              </p>
+            </NavLink>
+          </span>
+          <span className="uploadvideocomponent__buttons--cancel">Cancel</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default UploadVideoComponent;
